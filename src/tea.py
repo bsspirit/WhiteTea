9a0f1c70e3ffe9c20b3e881869c56e657cb547e8
Conan Zhang <bsspirit@163.com>
2011-04-21 00:30:46 +0800

    'init'

---
+++ src/tea.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, session, redirect, url_for, current_app
+
+#SERVER_PATH='http://dotabook.info/'
+#STATIC_PATH='http://1.dotabook.sinaapp.com/static/'
+
+#SERVER_PATH='http://localhost:5000/'
+#STATIC_PATH='http://localhost/static/'
+
+app = Flask(__name__)
+app.debug = True
+app.config.from_object(__name__)
+app.secret_key = 'A0Zr31j/3yX R~XHH!jmN]LWX/,?RT'
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+	
+@app.route('/photo')
+def photo():
+	return render_template('photo.html')
+
+@app.route('/consult')
+def consult():
+	return render_template('consult.html')
+	
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+if __name__ == '__main__':
+	app.run()
